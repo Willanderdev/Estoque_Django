dd7fd07771f4264d3dbc10c4486842c6bf1ad509
willanderdev <willanderdev@gmail.com>
2022-10-02 10:44:10 -0300

    correção

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z26g$o0bbwpu%1dknz9y+1-nbe6(3m6^b=n9&b450(b2+%q93)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ddah.herokuapp.com']
 
