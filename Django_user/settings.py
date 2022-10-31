2e7e7803db9ac68f2065aa61a5fd66dbd950d467
willanderdev <willanderdev@gmail.com>
2022-10-31 18:18:48 -0300

    corrigindo responsividade

--- Django_user/settings.py
+++ Django_user/settings.py
@@ -16,7 +16,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
--- staticfiles/js/user.js
+++
@@ -1,8 +0,0 @@
-
-$(document).on('click', "#btnImprimir", function () {
-    alert('aqui');
-    $.post('produto_del.html', function (retorno) {
-        $("#modalImprimir").modal({ backdrop: 'static' });
-        $("#tela").html(retorno);
-    });
-});
